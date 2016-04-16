a1e9c57d4b6359754078a70778c7c3ca6b639458
张猛 <511521437@qq.com>
2016-04-16 11:50:07 +0800

    ver1.0
    我的订单zm

---
+++ CLWsdht/MVC/User/Home/GoodModel.h
@@ -0,0 +1,22 @@
+//
+//  GoodModel.h
+//  CLWsdht
+//
+//  Created by koroysta1 on 16/4/13.
+//  Copyright © 2016年 时代宏图. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GoodModel : NSObject
+
+@property (nonatomic, copy) NSString *Cnt;
+@property (nonatomic, copy) NSString *Id;
+@property (nonatomic, copy) NSString *Img;//名称
+@property (nonatomic, copy) NSString *Name;
+@property (nonatomic, copy) NSString *PartsId;
+@property (nonatomic, copy) NSString *Price;
+@property (nonatomic, copy) NSString *StoreId;//名称
+@property (nonatomic, copy) NSString *Url;
+
+@end
